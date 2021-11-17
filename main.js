f73c0be32f9889103d57704a0db5016a6b45c8d8
Jitendra <jitendraks1803@gmail.com>
2021-11-17 15:23:20 +0530

    changes to blog page

--- main.js
+++ main.js
@@ -1,28 +1,32 @@
-const btn = document.querySelector(".main-btn")
-const newsletter = document.querySelector(".newsletter")
-const close = document.getElementById("close")
+const btn = document.querySelector(".main-btn");
+const newsletter = document.querySelector(".newsletter");
+const close = document.getElementById("close");
 
+btn.addEventListener("click", function () {
+  newsletter.classList.toggle("hidden");
+});
 
-console.log("hello");
-
-btn.addEventListener("click", function(){
-newsletter.classList.toggle("hidden")
-})
-
-close.addEventListener("click", function(){
-    newsletter.classList.add("hidden")
-
-})
-
+close.addEventListener("click", function () {
+  newsletter.classList.add("hidden");
+});
 
 let cats = document.getElementById("cats");
 let dogs = document.getElementById("dogs");
 let selectedOption;
-function destination() {
-  selectedOption = document.getElementById("selectDestination").value;
+function category() {
+  selectedOption = document.getElementById("selectCategory").value;
+  console.log(selectedOption);
+  if (selectedOption === "Cats") {
+    dogs.classList.add("hidden");
+    cats.classList.remove("hidden");
+  } else if (selectedOption === "Dogs") {
+    dogs.classList.remove("hidden");
+    cats.classList.add("hidden");
+  }
 
- console.log(selectedOption);
- 
+  else{
+    cats.classList.remove("hidden");
+    dogs.classList.remove("hidden");
+
+  }
 }
-
-
